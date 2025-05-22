33347f56f9d3709420e19960f6a1566deff8bd87
NikolozGirgvliani <nikolozgirgvliani29@gmail.com>
2025-05-22 23:19:55 +0400

    commit

---
+++ Level 97/script.js
@@ -0,0 +1,26 @@
+const img = document.getElementById('photo');
+const title = document.getElementById('title');
+
+function Width() {
+  img.style.width = '500px';
+}
+
+function Height() {
+  img.style.height = '400px';
+}
+
+function Radius() {
+  img.style.borderRadius = '25px';
+}
+
+function Shadow() {
+  img.style.boxShadow = '10px 10px 20px rgb(0, 0, 0)';
+}
+
+function TitleColor() {
+  title.style.color = 'blue';
+}
+
+function TitleSize() {
+  title.style.fontSize = '48px';
+}
